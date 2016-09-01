bc8b4df48f3d239b11408b4504ab9f971c667606
theasder <artem108980@gmail.com>
2016-09-01 15:44:27 +0300

    added post

--- finance/finance_project/personal_finance/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class PersonalFinanceConfig(AppConfig):
-    name = 'personal_finance'
--- finance/finance_project/personal_finance/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-
-from . import views
-
-app_name = 'interview_questions'
-urlpatterns = [
-    url(r'^$', views.IndexView.as_view(), name='index'),
-    url(r'^add/$', views.AddTransaction.as_view(), name='add'),
-]
