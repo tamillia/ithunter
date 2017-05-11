80ff84410719663fe993c4fd523a59b0f887e3ca
theasder <artem108980@gmail.com>
2017-05-12 02:41:48 +0300

    uploaded app

---
+++ js/app-in-one-file/app.js
@@ -0,0 +1,83 @@
+'use strict';
+var app = angular.module('app', []);
+
+app.controller('MenuCtrl', function($scope) {
+    $scope.custom = false;
+    $scope.toggleMenu = function () {
+        $scope.custom = !$scope.custom;
+    };
+});
+
+
+app.controller('main', function ($scope, $http) {
+
+    $scope.city = "";
+    $scope.language = "";
+    $scope.itemsPerPage = 10;
+    $scope.currentPage = 0;
+
+    $scope.cityFilter = function(proger) {
+        if ($scope.city === "") {
+            return true;
+        } else if (proger['location'] == null) {
+            return false;
+        } else if ($scope.city != "" && proger['location'] !== null) {
+            // console.log(proger['location'], $scope.city);
+            return proger['location'].indexOf($scope.city) !== -1;
+        } else {
+            return true;
+        }
+    }
+
+    $scope.langFilter = function(proger) {
+        if ($scope.language === "") {
+            return true;
+        } else if (proger['languages'] == null) {
+            return false;
+        } else if ($scope.language != "" && proger['languages'] !== null) {
+            console.log(proger['languages'], $scope.language + "'");
+            console.log(proger['languages'], $scope.language + ":");
+            return proger['languages'].indexOf($scope.language + "'") !== -1 || 
+                   proger['languages'].indexOf($scope.language + ':') !== -1;
+        } else {
+            return true;
+        }
+    }
+
+    function loadPage(page) {
+        $http.get('data/data.json')
+            .then(function(res){
+                // $scope.len = res.data.length;
+                $scope.progers = res.data; //.slice(page * $scope.itemsPerPage, (page + 1) * $scope.itemsPerPage);
+        });
+    }
+    
+
+    loadPage(0);
+
+    // $scope.firstPage = function() {
+    //     return $scope.currentPage == 0;
+    // };
+
+    // $scope.lastPage = function() {
+    //     var lastPageNum = Math.ceil($scope.len / $scope.itemsPerPage - 1);
+    //     return $scope.currentPage == lastPageNum || $scope.currentPage == lastPageNum + 1;
+    // };
+
+    // $scope.numberOfPages = function() {
+    //     return Math.ceil($scope.len / $scope.itemsPerPage);
+    // };
+
+    // $scope.startingItem = function() {
+    //     return $scope.currentPage * $scope.itemsPerPage;
+    // };
+    // $scope.pageBack = function() {
+    //     $scope.currentPage = $scope.currentPage - 1;
+    //     loadPage($scope.currentPage);
+    // };
+
+    // $scope.pageForward = function() {
+    //     $scope.currentPage = $scope.currentPage + 1;
+    //     loadPage($scope.currentPage);
+    // };
+});
